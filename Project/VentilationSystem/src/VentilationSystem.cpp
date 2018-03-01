b65b041378b50609270460638f9e7ebb04de6d7f
Nhan Phan - Laptop <Usin2705@yahoo.com>
2018-03-01 12:14:00 +0200

    feat: add fancontrol hardcoded (upload the mai file)

--- Project/VentilationSystem/src/VentilationSystem.cpp
+++ Project/VentilationSystem/src/VentilationSystem.cpp
@@ -6,7 +6,7 @@
  Copyright   : $(copyright)
  Description : main definition
 ===============================================================================
-*/
+ */
 
 #if defined (__USE_LPCOPEN)
 #if defined(NO_BOARD_LIB)
@@ -18,31 +18,174 @@
 
 #include <cr_section_macros.h>
 
-// TODO: insert other include files here
+#include "ModbusMaster.h"
 
-// TODO: insert other definitions and declarations here
+using namespace std;
 
+#define TICKRATE_HZ1 1000
+static volatile int counter;
+static volatile uint32_t systicks;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+ * @brief	Handle interrupt from SysTick timer
+ * @return	Nothing
+ */
+void SysTick_Handler(void)
+{
+	systicks++;
+	if(counter > 0) counter--;
+}
+#ifdef __cplusplus
+}
+#endif
+
+void Sleep(int ms)
+{
+	counter = ms;
+	while(counter > 0) {
+		__WFI();
+	}
+}
+
+/* this function is required by the modbus library */
+uint32_t millis() {
+	return systicks;
+}
+
+void printRegister(ModbusMaster& node, uint16_t reg) {
+	uint8_t result;
+	// slave: read 16-bit registers starting at reg to RX buffer
+	result = node.readHoldingRegisters(reg, 1);
+
+	// do something with data if read is successful
+	if (result == node.ku8MBSuccess)
+	{
+		printf("R%d=%04X\n", reg, node.getResponseBuffer(0));
+	}
+	else {
+		printf("R%d=???\n", reg);
+	}
+}
+
+bool setFrequency(ModbusMaster& node, uint16_t freq) {
+	uint8_t result;
+	int ctr;
+	bool atSetpoint;
+	const int delay = 500;
+
+	node.writeSingleRegister(1, freq); // set motor frequency
+
+	printf("Set freq = %d\n", freq/40); // for debugging
+
+	// wait until we reach set point or timeout occurs
+	ctr = 0;
+	atSetpoint = false;
+	do {
+		Sleep(delay);
+		// read status word
+		result = node.readHoldingRegisters(3, 1);
+		// check if we are at setpoint
+		if (result == node.ku8MBSuccess) {
+			if(node.getResponseBuffer(0) & 0x0100) atSetpoint = true;
+		}
+		ctr++;
+	} while(ctr < 20 && !atSetpoint);
+
+	printf("Elapsed: %d\n", ctr * delay); // for debugging
+
+	return atSetpoint;
+}
+
+/*
+ * Running the fan
+ * Input: speed of the fan. Range from 0 (0Hz) to 20000 (50Hz). 400units/Hz
+ * Output: void
+ *
+ * The fan need the Sleep function and other Modbus library to work
+ *
+ *
+ */
+
+void runFan(int speed) {
+	ModbusMaster node(2); // Create modbus object that connects to slave id 2
+
+	node.begin(9600); // set transmission rate - other parameters are set inside the object and can't be changed here
+
+	printRegister(node, 3); // for debugging
+
+	node.writeSingleRegister(0, 0x0406); // prepare for starting
+
+	printRegister(node, 3); // for debugging
+
+	Sleep(1000); // give converter some time to set up
+	// note: we should have a startup state machine that check converter status and acts per current status
+	//       but we take the easy way out and just wait a while and hope that everything goes well
+
+	printRegister(node, 3); // for debugging
+
+	node.writeSingleRegister(0, 0x047F); // set drive to start mode
+
+	printRegister(node, 3); // for debugging
+
+	Sleep(1000); // give converter some time to set up
+	// note: we should have a startup state machine that check converter status and acts per current status
+	//       but we take the easy way out and just wait a while and hope that everything goes well
+
+	printRegister(node, 3); // for debugging
+
+	int j = 0;
+	//const uint16_t fa[20] = { 1000, 2000, 3000, 3500, 4000, 5000, 7000, 8000, 8300, 10000, 10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000 };
+
+	while (1) {
+		uint8_t result;
+
+		// slave: read (2) 16-bit registers starting at register 102 to RX buffer
+		j = 0;
+		do {
+			result = node.readHoldingRegisters(102, 2);
+			j++;
+		} while(j < 3 && result != node.ku8MBSuccess);
+		// note: sometimes we don't succeed on first read so we try up to threee times
+		// if read is successful print frequency and current (scaled values)
+		if (result == node.ku8MBSuccess) {
+			printf("F=%4d, I=%4d  (ctr=%d)\n", node.getResponseBuffer(0), node.getResponseBuffer(1),j);
+		}
+		else {
+			printf("ctr=%d\n",j);
+		}
+
+		Sleep(3000);
+
+		// frequency is scaled:
+		// 20000 = 50 Hz, 0 = 0 Hz, linear scale 400 units/Hz
+		// Set the fan speed
+		setFrequency(node, speed);
+	}
+}
 int main(void) {
 
 #if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
+	// Read clock settings and update SystemCoreClock variable
+	SystemCoreClockUpdate();
 #if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
+	// Set up and initialize all required blocks and
+	// functions related to the board hardware
+	Board_Init();
+	// Set the LED to the state of "On"
+	Board_LED_Set(0, true);
 #endif
 #endif
 
-    // TODO: insert code here
+	/* The sysTick counter only has 24 bits of precision, so it will
+				   overflow quickly with a fast core clock. You can alter the
+				   sysTick divider to generate slower sysTick clock rates. */
+	Chip_Clock_SetSysTickClockDiv(1);
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
-    return 0 ;
+	SysTick_Config(Chip_Clock_GetSysTickClockRate()/TICKRATE_HZ1);
+
+	//May be 19999 is the maximum (for the fan to run without stop)
+	runFan(19999);
 }
